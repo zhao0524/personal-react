0214b2b5b79dc981b4b6de50cd8c6afc77d47f7c
David Zhao <115097081+Kamisatodavid@users.noreply.github.com>
2025-08-22 19:04:18 -0400

    changed the nav bar error

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { Home } from "./pages/home";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import { NotFound } from "./pages/Notfound";
 import { Toaster } from "./components/ui/toaster";
 
 function App() {
   return (
     <>
       <Toaster />
-      <HashRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
+      <Home />
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setIsScrolled(window.screenY > 10);
+      setIsScrolled(window.scrollY > 10);
     };
 
     window.addEventListener("scroll", handleScroll);
