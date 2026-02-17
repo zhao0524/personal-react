17d642d0d8dcb8e1123884311fd1a8925394f6ac
David Zhao <115097081+Kamisatodavid@users.noreply.github.com>
2026-02-17 12:17:10 -0500

    fixed vercel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { ThemeToggle } from "../components/Themetoggle";
+import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
 import { Navbar } from "../components/Navbar";
 import { HeroSection } from "../components/HeroSection";
