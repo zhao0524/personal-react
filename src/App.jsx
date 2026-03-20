081aa81d9ccc6610fe52bb3a116b2e95bc3a5a1d
David Zhao <115097081+Kamisatodavid@users.noreply.github.com>
2026-03-20 12:57:22 -0400

    added click spark

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Home } from "./pages/Home";
 import { Toaster } from "./components/ui/toaster";
+import ClickSpark from "./components/ClickSpark";
 
 function App() {
   return (
-    <>
+    <ClickSpark
+      sparkColor="#fff"
+      sparkSize={14}
+      sparkRadius={45}
+      sparkCount={8}
+      duration={400}
+    >
       <Toaster />
       <Home />
-    </>
+    </ClickSpark>
   );
 }
 
